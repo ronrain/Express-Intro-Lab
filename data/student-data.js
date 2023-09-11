1fe5f58be96f7c2a29d6c740a4ea3dcca0c43fe6
RonRain <rarainero@gmail.com>
2023-09-11 18:57:54 -0400

    lab complete

---
+++ data/student-data.js
@@ -0,0 +1,14 @@
+const students = [
+  {name: 'Haley', job: true, _id: 125223},
+  {name: 'Andrew', job: true, _id: 127904},
+  {name: 'Anthony', job: true, _id: 139608},
+  {name: 'Joe', job: true, _id: 123342},
+  {name: 'Augie', job: true, _id: 134455},
+  {name: 'Ron', job: false, _id:111111},
+  {name: 'Lindsey', job: true, _id:123445},
+  {name: 'Sarah', job: true, _id:127899},
+]
+
+export { 
+	students
+}
